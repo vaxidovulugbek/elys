fa3747e161166a8df3c2a25f8cb7a92886453d26
Abdulaziz <navoiy498@gmail.com>
2022-10-18 13:18:48 +0500

    pruduction bug fix

--- src/services/functions/index.js
+++ src/services/functions/index.js
@@ -41,32 +41,40 @@ const apartmentStatusInPiece = (apartments) => {
 		status_not_for_sale: 0,
 		status_construction: 0,
 	};
-
-	for (let i = 0; i < apartments.length; i++) {
-		if (apartments[i].status === 0) statuses.status_inactive++;
-		if (apartments[i].status === 1) statuses.status_free++;
-		if (apartments[i].status === 2) statuses.status_interest++;
-		if (apartments[i].status === 3) statuses.status_sold++;
-		if (apartments[i].status === 4) statuses.status_not_for_sale++;
-		if (apartments[i].status === 5) statuses.status_construction++;
+	if (isArray(apartments)) {
+		for (let i = 0; i < apartments.length; i++) {
+			if (apartments[i]?.status === 0) statuses.status_inactive++;
+			if (apartments[i]?.status === 1) statuses.status_free++;
+			if (apartments[i]?.status === 2) statuses.status_interest++;
+			if (apartments[i]?.status === 3) statuses.status_sold++;
+			if (apartments[i]?.status === 4) statuses.status_not_for_sale++;
+			if (apartments[i]?.status === 5) statuses.status_construction++;
+		}
 	}
-
 	return statuses;
 };
 
 const apartmentStatusInPercent = (apartments) => {
-	const apartments_count = apartments.length;
-	const statuses = apartmentStatusInPiece(apartments);
-	const statuses_in_percent = {
-		status_inactive: toFixed(100 / (apartments_count / statuses.status_inactive), 2),
-		status_free: toFixed(100 / (apartments_count / statuses.status_free), 2),
-		status_interest: toFixed(100 / (apartments_count / statuses.status_interest), 2),
-		status_sold: toFixed(100 / (apartments_count / statuses.status_sold), 2),
-		status_not_for_sale: toFixed(100 / (apartments_count / statuses.status_not_for_sale), 2),
-		status_construction: toFixed(100 / (apartments_count / statuses.status_construction), 2),
-	};
+	if (isArray(apartments)) {
+		const apartments_count = apartments.length;
+		const statuses = apartmentStatusInPiece(apartments);
+		const statuses_in_percent = {
+			status_inactive: toFixed(100 / (apartments_count / statuses.status_inactive), 2),
+			status_free: toFixed(100 / (apartments_count / statuses.status_free), 2),
+			status_interest: toFixed(100 / (apartments_count / statuses.status_interest), 2),
+			status_sold: toFixed(100 / (apartments_count / statuses.status_sold), 2),
+			status_not_for_sale: toFixed(
+				100 / (apartments_count / statuses.status_not_for_sale),
+				2
+			),
+			status_construction: toFixed(
+				100 / (apartments_count / statuses.status_construction),
+				2
+			),
+		};
 
-	return statuses_in_percent;
+		return statuses_in_percent;
+	}
 };
 
 const translateConstans = (t, data) => {
