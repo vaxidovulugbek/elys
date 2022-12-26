df6cdbd8890b1d00585cd42e85226e73847d573f
Abdumajid  Rashidov <abdumajidrashidov44@gmail.com>
2022-12-26 16:57:38 +0500

    bug fixed

--- src/modules/Settings/components/ClientViewTable.jsx
+++ src/modules/Settings/components/ClientViewTable.jsx
@@ -1,68 +1,121 @@
-import { Table } from "components";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { time } from "services";
 
-export const ClientViewTable = ({ items }) => {
+export const ClientViewTable = ({ items = [] }) => {
+	const { t } = useTranslation();
 	return (
-		<Table
-			columns={[
-				{
-					title: "ID",
-					dataKey: "id",
-					render: (value) => value,
-				},
-				{
-					title: "First name",
-					dataKey: "first_name",
-					render: (value) => value,
-				},
-				{
-					title: "Middle name",
-					dataKey: "middle_name",
-					render: (value) => value,
-				},
-				{
-					title: "Last name",
-					dataKey: "last_name",
-					render: (value) => value,
-				},
-				{
-					title: "Address",
-					dataKey: "address",
-					render: (value) => value,
-				},
-				{
-					title: "Birthdate",
-					dataKey: "birthdate",
-					render: (value) => time.to(value),
-				},
-				{
-					title: "Email",
-					dataKey: "mail",
-					render: (value) => value,
-				},
-				{
-					title: "Phone",
-					dataKey: "phone",
-					render: (value) => value,
-				},
-				{
-					title: "Passport",
-					dataKey: "passport",
-					render: (value) => value,
-				},
-				{
-					title: "Passport issued by",
-					dataKey: "passport_issued_by",
-					render: (value) => value,
-				},
-				{
-					title: "Passport issued date",
-					dataKey: "passport_issued_date",
-					render: (value) => time.to(value),
-				},
-			]}
-			items={items.length && items}
-		/>
+		<>
+			{items?.map((client, index) => {
+				return (
+					<div className="card " key={index}>
+						<div className="invoice__modal">
+							<div className="invice__modal-content row">
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>ID</span>
+
+										<input
+											type="text"
+											style={{ width: "100%" }}
+											disabled
+											value={client.id}
+										/>
+									</label>
+								</div>
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>{t("Name")}</span>
+										<input
+											type="text"
+											style={{ width: "100%" }}
+											disabled
+											value={`${client.first_name} ${client.last_name} ${client.middle_name}`}
+										/>
+									</label>
+								</div>
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>{t("address")}</span>
+										<input
+											style={{ width: "100%" }}
+											type="text"
+											disabled
+											value={client.address}
+										/>
+									</label>
+								</div>
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>{t("birthdate")}</span>
+										<input
+											style={{ width: "100%" }}
+											type="text"
+											disabled
+											value={time.to(client.birthdate)}
+										/>
+									</label>
+								</div>
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>{t("email")}</span>
+										<input
+											type="text"
+											style={{ width: "100%" }}
+											disabled
+											value={client.mail}
+										/>
+									</label>
+								</div>
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>{t("passport")}</span>
+										<input
+											type="text"
+											style={{ width: "100%" }}
+											disabled
+											value={client.passport}
+										/>
+									</label>
+								</div>
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>{t("passport_issued_by")}</span>
+										<input
+											type="text"
+											disabled
+											style={{ width: "100%" }}
+											value={client.passport_issued_by}
+										/>
+									</label>
+								</div>
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>{t("passport_issued_date")}</span>
+										<input
+											type="text"
+											style={{ width: "100%" }}
+											disabled
+											value={time.to(client.passport_issued_date)}
+										/>
+									</label>
+								</div>
+								<div className="invoce__form-wrapper col-4">
+									<label>
+										<span>{t("phone")}</span>
+										<input
+											style={{ width: "100%" }}
+											type="text"
+											disabled
+											value={client.phone}
+										/>
+									</label>
+								</div>
+							</div>
+						</div>
+					</div>
+				);
+			})}
+		</>
 	);
 };
